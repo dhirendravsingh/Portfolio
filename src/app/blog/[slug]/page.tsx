7d9772c9d94735a7a4705c1cd865a7d29f257788
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-07-11 21:56:52 +0530

    enhanced styling and structuing of blog page/ added another blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,24 +4,32 @@ import Container from '@/components/container';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import {promises as fs} from 'fs'
 import path from 'path';
-import { compileMDX } from 'next-mdx-remote/rsc';
+import { getSingleBlog } from '@/utils/mdx';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: "All blogs - Dhirendra Vikram Singh",
   description: "All my general wisdom and thoughts",
 };
 
-export default async function SingleBlogPage() {
+export default async function SingleBlogPage({params} : {
+  params : {
+    slug : string
+  }
+}) {
+    const slug = params.slug;
+    const blog= await getSingleBlog(slug);
 
-    const singleBlog = await fs.readFile(path.join(process.cwd(), 'src/data', 'tail.mdx'), 'utf-8');
-    const {content, frontmatter} = await compileMDX<{title: string}>({
-        source : singleBlog,
-        options : {parseFrontmatter: true}
-    })
+    if(!blog) {
+      redirect("/blog")
+    }
+
+    
+  const {content, frontmatter} = blog;
   return (
     <div className="min-h-screen flex items-start justify-start">
       <Container className="min-h-[200vh] p-4 md:pt-20 md:pb-10">
-        <div className='prose'>
+        <div className='prose mx-auto'>
         {content}
         </div>
       </Container>
---
+++ src/utils/mdx.ts
@@ -0,0 +1,22 @@
+import {promises as fs }from "fs"
+import path from "path"
+import { compileMDX } from 'next-mdx-remote/rsc';
+
+export const getSingleBlog = async (slug : string) => {
+    try {
+        const singleBlog = await fs.readFile(
+        path.join(process.cwd(), "src/data", `${slug}.mdx`), "utf-8"
+    )
+    if(!singleBlog) { 
+        null
+    }
+      const {content, frontmatter} = await compileMDX<{title: string}>({
+        source : singleBlog,
+        options : {parseFrontmatter: true}
+    })
+    return {content, frontmatter};
+    } catch (error) {
+        console.error(`Error reading blog from slug ${slug}`, error);
+        return null
+    }
+}
